f2157e9214f67c4a4806c977d17a05ae92a76182
xiaofen <oulafen@gmail.com>
2014-04-19 14:17:41 +0800

    Rewrite the function of BidPeople.prototype.update with mongoose

--- models/bid_people.js
+++ models/bid_people.js
@@ -1,23 +1,29 @@
-var mongodb = require('./db');
+//var mongodb = require('./db');
+var mongoose = require('mongoose');
+
+var bidPeopleSchema = new mongoose.Schema({
+    user: String,
+    bid_people: Array
+}, {
+    collection: 'bid_people'
+});
+
+var bidPeopleModel = mongoose.model('BidPeople', bidPeopleSchema);
 
 function BidPeople(user,bid_people) {
     this.user = user;
     this.bid_people = bid_people ;
 }
 
-module.exports = BidPeople;
-
 BidPeople.prototype.update = function(callback){
-    var bid_people = this;
-    mongodb.open(function (err, db) {
-        db.collection('bid_people', function (err, collection) {
-            collection.remove({user:bid_people.user},function(err,bid_people){
-                return callback(null,bid_people);
-            });
-            collection.insert(bid_people,{safe: true},function(err,bid_people){
-                mongodb.close();
-                return callback(null,bid_people);
-            })
-        });
+    var bid_people = new bidPeopleModel(this);
+    bidPeopleModel.remove({user:this.user},function(err,bid_people){
+        callback(null,bid_people);
+    });
+    bid_people.save(bid_people,function(err,bid_people){
+        callback(null,bid_people);
     });
 };
+
+module.exports = BidPeople;
+
